22754fc83b1f81a5b26a53bf90cee9439d0919fd
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-08-03 17:48:32 +0200

    Bottom dock customization done

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,29 +1,32 @@
 "use client";
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import React, { useEffect } from "react";
+
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { IconMoon, IconSun } from "@tabler/icons-react";
 
 export function ToggleTheme() {
-   const { setTheme } = useTheme();
+   const { theme, setTheme } = useTheme();
+   useEffect(() => {
+      setTheme("system");
+   }, [setTheme]);
+
+   const toggleTheme = () => {
+      if (theme === "system") {
+         const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "light" : "dark";
+         setTheme(systemTheme);
+      } else {
+         setTheme("system");
+      }
+   };
 
    return (
-      <DropdownMenu>
-         <DropdownMenuTrigger asChild>
-            <Button variant='outline' size='icon'>
-               <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-               <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-               <span className='sr-only'>Toggle theme</span>
-            </Button>
-         </DropdownMenuTrigger>
-         <DropdownMenuContent align='end'>
-            <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-         </DropdownMenuContent>
-      </DropdownMenu>
+      <Button variant='outline' size='icon' onClick={toggleTheme} className='border-none rounded-full'>
+         <IconSun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+         <IconMoon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+         <span className='sr-only'>Toggle theme</span>
+      </Button>
    );
 }
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+const NavData = {
+   navbar: [
+      { href: "#", icon: "IconBraces", label: "Ejercicio 1" },
+      { href: "#", icon: "IconBrackets", label: "Ejercicio 2" },
+   ],
+};
+export default NavData;
